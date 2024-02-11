8fb3e77d31447f3d87b6e5df207ccad7bd6f2d54
Flame <Flame_ustc@hotmail.com>
2024-02-11 11:52:45 +0800

    Setup Github page

---
+++ loader.user.js
@@ -0,0 +1,25 @@
+// ==UserScript==
+// @name BCRevamp-Lilian (Loader)
+// @namespace https://www.bondageprojects.com/
+// @version 1.0
+// @description Bondage Club Revamp by Lilian Script Loader
+// @author Lilian
+// @match bondageprojects.elementfx.com/*/BondageClub/*
+// @match www.bondageprojects.elementfx.com/*/BondageClub/*
+// @match bondage-europe.com/*/BondageClub/*
+// @match www.bondage-europe.com/*/BondageClub/*
+// @grant none
+// @run-at document-end
+// ==/UserScript==
+
+(function () {
+    "use strict";
+    const src = `'http://localhost:3042/undefined/Revamp/main.js?v=${Date.now()}`;
+    if (typeof BCRevampLilian_Loaded === "undefined") {
+        const n = document.createElement("script");
+        n.setAttribute("type", "text/javascript");
+        n.setAttribute("src", src);
+        n.setAttribute("id", "BCRevampLilianLoader");
+        document.head.appendChild(n);
+    }
+})();
